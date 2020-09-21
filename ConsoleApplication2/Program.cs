a96e547c54edb2c9c0be5d3a5063330afb38ace4
Seyda1 <seydaybar@gmail.com>
2020-09-21 09:42:47 +0300

    initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            char islem;
+            int s1, s2, sonuc = 0;
+            Console.WriteLine("aritmetik işlemi(+,-,*) ve iki sayı giriniz");
+           islem= Convert.ToChar(Console.ReadLine());
+            s1=Convert.ToInt32(Console.ReadLine());
+            s2=Convert.ToInt32(Console.ReadLine());
+            switch(islem)
+            {
+                case '+':
+                    sonuc=s1+s2;
+                    break;
+                case '-':
+                    sonuc=s1-s2;
+                    break;
+                case'*':
+                    sonuc = s1 * s2;
+                    break;
+                
+
+            }
+            Console.WriteLine("sonuc={0}",sonuc);
+           
+            Console.ReadLine();
+        }
+        
+    }
+
